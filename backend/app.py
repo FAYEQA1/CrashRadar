5b443c58ad42898715f30f10d7df0f5dab9261e8
Humera-tech <hmahreen3105@gmail.com>
2026-05-29 22:11:03 +0530

    backend

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
-from flask import Flask , render_template,request,url_for
+from flask import Flask , render_template,request,url_for,jsonify
 import os 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('')
+    return jsonify({
+        "message": "Crash Radar Backend Running"
+    })
+
+@app.route('/detection',methods = ["POST"])
+def detect():
+    return jsonify({
+        "status": "success",
+        "result": "Crash detected"
+    })
 
 if __name__ =='__main__':
     app.run(debug=True)
